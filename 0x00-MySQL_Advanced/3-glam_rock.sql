f55aa0dca2fd507f5a47c296044b10734f3b80ff
Reliccode <bryanmaritim@gmail.com>
2024-01-18 12:41:59 +0300

    script defines a function calculate_lifespan to compute the lifespan based on the formed and split years.

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,24 +1,15 @@
 -- 3-glam_rock.sql
 
--- Set the delimiter to handle semicolons within the script
-DELIMITER $$
-
--- Create a function to calculate the lifespan
-CREATE FUNCTION calculate_lifespan(formed_year INT, split_year INT) RETURNS INT
-BEGIN
-    DECLARE lifespan INT;
-    SET lifespan = IF(split_year = 0, 2022 - formed_year, split_year - formed_year);
-    RETURN lifespan;
-END$$
-
--- Reset the delimiter
-DELIMITER ;
-
 -- Use the database
 USE holberton;
 
 -- Select the bands with Glam rock as their main style, ranked by longevity
-SELECT band_name, calculate_lifespan(formed, split) AS lifespan
+SELECT
+    band_name,
+    CASE
+        WHEN split = 0 THEN 2022 - formed
+        ELSE split - formed
+    END AS lifespan
 FROM bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;

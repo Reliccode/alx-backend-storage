9f5116d28e8b78f5d175fa9f9314e569eea103a5
Reliccode <bryanmaritim@gmail.com>
2024-01-24 22:17:21 +0300

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python3
-"""use requests module to obtain the HTML content of a particular URL and returns it"""
+"""Expiring web cache and tracker module"""
 
 import redis
 import requests
 from typing import Callable
 from functools import wraps
 
+redis_connection = redis.Redis()
 
-def access(method: Callable) -> Callable:
-    """decorator for get_page"""
+
+def count_requests(method: Callable) -> Callable:
+    """
+    Decorator that wraps around HTTP request functions to provide caching.
+    """
+
     @wraps(method)
-    def count(url: str) -> str:
-        """track how many times a particular URL was accessed"""
-        redis_client = redis.Redis()
-        redis_client.incr(f'count:{url}')
-        cached = redis_client.get(f'cached:{url}')
-        if cached:
-            return cached.decode('utf-8')
-        res = method(url)
-        redis_client.setex(f'cached:{url}', 10, res)
-        return res
-    return count
+    def wrapper(url):
+        """Wrapper function for the count_requests decorator."""
+        redis_connection = redis.Redis()
+        redis_connection.incr(f"count:{url}")
+        cached_response = redis_connection.get(url)
+        if cached_response:
+            return cached_response.decode("utf-8")
+        response = method(url)
+        redis_connection.set(url, response, 10)
+        return response
+
+    return wrapper
 
 
-@access
+@count_requests
 def get_page(url: str) -> str:
-    """send request to url"""
-    return requests.get(url).text
-
-
-if __name__ == '__main__':
-    get_page('http://slowwly.robertomurray.co.uk')
+    """
+    Retrieves a web page, and caches the response for 10 seconds.
+    """
+    response = requests.get(url)
+    return response.text

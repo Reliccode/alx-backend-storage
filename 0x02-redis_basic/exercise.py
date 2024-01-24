fef223ae269de531f2424315447775f48a2ab963
Reliccode <bryanmaritim@gmail.com>
2024-01-24 21:42:58 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """
 Cache class for writing strings to Redis
 """
+from typing import Callable, Union
 import redis
 import uuid
 from typing import Union
@@ -34,3 +35,72 @@ if __name__ == "__main__":
     print(key)
     local_redis = redis.Redis()
     print(local_redis.get(key))
+
+# Reading from Redis and recovering original type
+    """
+Cache class for reading from Redis and recovering original type
+"""
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+
+    def get(self,
+            key: str,
+            fn: Callable = None) -> Union[str,
+                                          bytes,
+                                          int,
+                                          float]:
+        """
+        Retrieve data from Redis using the specified key
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+            fn (Callable): Optional callable function to convert the data.
+
+        Returns:
+            Union: Retrieved data, possibly converted by the provided function.
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Shortcut method to retrieve a string from Redis.
+
+        Args:
+            key (str): The key to retrieve the string from Redis.
+
+        Returns:
+            str: Retrieved string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Shortcut method to retrieve an integer from Redis.
+
+        Args:
+            key (str): The key to retrieve the integer from Redis.
+
+        Returns:
+            int: Retrieved integer.
+        """
+        return self.get(key, fn=int)
+
+
+if __name__ == "__main__":
+    cache = Cache()
+
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
